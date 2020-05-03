6bdcb3a4dc9e03f20b0e4c43fe362a759440e00e
brownard <brownard@live.co.uk>
2020-05-03 14:35:25 +0100

    Simplify Movies routes

--- MP2Web/ClientApp/src/app/movies/movies.module.ts
+++ MP2Web/ClientApp/src/app/movies/movies.module.ts
@@ -7,14 +7,12 @@ import { AppCommonModule } from '../common/modules/app-common.module';
 import { VideoPlayerModule } from '../video-player/video-player.module';
 import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
 import { MovieListComponent } from './components/movie-list/movie-list.component';
-import { MoviesComponent } from './components/movies.component';
 import { MoviesEffects } from './store/movies.effects';
-import { reducer } from './store/movies.reducers';
-import { featureKey } from './store/movies.state';
+import * as MoviesReducers from './store/movies.reducers';
+import * as MoviesState from './store/movies.state';
 
 @NgModule({
   declarations: [
-    MoviesComponent,
     MovieListComponent,
     MovieDetailsComponent
   ],
@@ -22,18 +20,13 @@ import { featureKey } from './store/movies.state';
     CommonModule,
     AppCommonModule,
     VideoPlayerModule,
-    StoreModule.forFeature(featureKey, reducer),
+    StoreModule.forFeature(MoviesState.featureKey, MoviesReducers.reducer),
     EffectsModule.forFeature([MoviesEffects]),
     RouterModule.forChild([
-      {
-        path: '', component: MoviesComponent, data: { animation: 'MoviesPage' },
-        children: [
-          { path: '', redirectTo: 'list', pathMatch: 'full' },
-          { path: 'list', component: MovieListComponent, data: { animation: 'MovieListPage' } },
-          { path: ':id', component: MovieDetailsComponent, data: { animation: 'MovieDetailsPage' } }
-        ]
-      }
-      ])
+      { path: '', redirectTo: 'list', pathMatch: 'full' },
+      { path: 'list', component: MovieListComponent, data: { animation: 'MovieListPage' } },
+      { path: ':id', component: MovieDetailsComponent, data: { animation: 'MovieDetailsPage' } }
+    ])
   ]
 })
 export class MoviesModule { }
