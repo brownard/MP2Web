78b4ffd9f39f51f58668ef13dc56aa5086a5d9c7
brownard <brownard@live.co.uk>
2020-07-07 02:17:12 +0100

    Restore finish stream on window unload

--- MP2Web/ClientApp/src/app/app.component.ts
+++ MP2Web/ClientApp/src/app/app.component.ts
@@ -67,4 +67,13 @@ export class AppComponent implements OnInit, OnDestroy {
     return false;
     //return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
   }
+
+  // Fallback that tries to stop any ongoing trancoding on the server when the page refreshes/closes
+  @HostListener('window:beforeunload', ['$event'])
+  beforeunloadHandler($event) {
+    let config = this.appConfig.appConfig;
+    let url = config.mp2ExtendedBasePath + config.streamingServicePath + 'FinishStream?identifier=' + this.appConfig.appInstanceId;
+    let result = fetch(url, { keepalive: true }); //navigator.sendBeacon(url);
+    return;
+  }
 }
